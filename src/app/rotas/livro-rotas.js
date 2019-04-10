f92f4267d2b68ba32f7792b4f0eb118b30d56d13
rsaless <rspavarina@gmail.com>
2019-04-10 17:50:39 -0300

    implementado sistema de autorização

--- src/app/rotas/livro-rotas.js
+++ src/app/rotas/livro-rotas.js
@@ -1,11 +1,12 @@
 const Livro = require('../models/livro');
-
+const BaseControlador = require('../controllers/base-controlador');
 const LivroControlador = require('../controllers/livro-controlador');
 const livroControlador = new LivroControlador();
 
 module.exports = (app) => {
     const rotasLivro = LivroControlador.rotas();
 
+    app.use(rotasLivro.autenticadas, (req, res, next) => (req.isAuthenticated()) ? next() : res.redirect(BaseControlador.rotas().login))
     app.get(rotasLivro.lista, livroControlador.lista());
     app.get(rotasLivro.edicao, livroControlador.formularioEdicao());
     app.route(rotasLivro.cadastro)
